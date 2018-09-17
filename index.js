fe41baab211db0dd5987b5f10de7dee560efab58
杨艾林 <747564962@qq.com>
2018-09-17 14:47:26 +0800

    compatible with webpack4

--- index.js
+++ index.js
@@ -14,40 +14,51 @@ class cssPlugin {
   constructor(config) {
     this.config = config || defaultConfig ;
   }
+  pluginHandler(compilation, callback){
+    // merge the sanme media query
+    const mergeMedia = (content,key)=>{
+      return pipline.process(content).then(res=>{
+       return duplicateRemove(res,key)
+       })
+     }
+     //use cssnano to remove the same css style
+     const duplicateRemove = (content,key)=>{
+       return cssnano.process(content,this.config).then(res=>{
+         compilation.assets[key] = {
+           source: () => res.css,
+             // return file size
+           size: () => {
+             return Buffer.byteLength(res.css, 'utf8')
+           }
+         }
+       })
+     }
+     let css = Object.keys(compilation.assets).filter(item=>item.includes('.css'))
+     const promiseArr = css.map(key=>{
+       const fileContent = compilation.assets[key].source();
+       return new Promise((resolve,reject)=>{
+         mergeMedia(fileContent,key).then(
+           res=>resolve(res),
+           err=>reject(err))
+       })
+     })
+     Promise.all(promiseArr).then((res)=>{
+       callback();
+     })
+  }
   apply(compiler) {
-    compiler.plugin('emit', (compilation, callback) => {
-      // merge the sanme media query
-      const mergeMedia = (content,key)=>{
-       return pipline.process(content).then(res=>{
-        return duplicateRemove(res,key)
+    try {
+      if('hooks' in compiler){
+        compiler.hooks.emit.tapAsync('pluginHandlerAsync',(compilation, callback)=>{
+          this.pluginHandler(compilation, callback)
         })
+      }else{
+        compiler.plugin('emit', (compilation, callback) => {
+         this.pluginHandler(compilation, callback)
+        });
       }
-      //use cssnano to remove the same css style
-      const duplicateRemove = (content,key)=>{
-        return cssnano.process(content,this.config).then(res=>{
-          compilation.assets[key] = {
-            source: () => res.css,
-              // return file size
-            size: () => {
-              return Buffer.byteLength(res.css, 'utf8')
-            }
-          }
-        })
-      }
-      let css = Object.keys(compilation.assets).filter(item=>item.includes('.css'))
-      const promiseArr = css.map(key=>{
-        const fileContent = compilation.assets[key].source();
-        return new Promise((resolve,reject)=>{
-          mergeMedia(fileContent,key).then(
-            res=>resolve(res),
-            err=>reject(err))
-        })
-      })
-      Promise.all(promiseArr).then((res)=>{
-        callback();
-      })
-      
-    });
+    } catch (error) {
+    }
   }
 }
 module.exports = cssPlugin;
